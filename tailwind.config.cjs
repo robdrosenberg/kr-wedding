d622a6039e24a7a8875625bb5a49b22dc7a3380f
Scott Stanfel <scottstanfel@Scotts-MacBook-Pro.local>
2023-05-10 13:05:58 +0900

    add details section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       backgroundColor: {
-        'nav': '#B7BDB8',
-        'primary': '#C3C9C5',
-        'polaroid': '#FDFDFD',
+        nav: "#B7BDB8",
+        primary: "#C3C9C5",
+        polaroid: "#FDFDFD",
       },
       boxShadow: {
-        'polaroid': '0px 2px 4px rgba(49, 54, 50, 0.49)',
+        polaroid: "0px 2px 4px rgba(49, 54, 50, 0.49)",
       },
       colors: {
-        'darkest': '#313632'
+        darkest: "#313632",
+        "green-dark": "#AAB1AA",
+        green: "#B7BDB8",
       },
       fontFamily: {
-        'bell': ["Bellissima Script Pro", 'cursive'],
-        'mack': ['p22-mackinac-pro', 'serif'],
-        'gothic-w': ['tablet-gothic-wide', 'sans-serif'],
+        bell: ["Bellissima Script Pro", "cursive"],
+        mack: ["p22-mackinac-pro", "serif"],
+        "gothic-w": ["tablet-gothic-wide", "sans-serif"],
       },
-      
+      aspectRatio: {
+        "4/3": "4 / 3",
+      },
     },
-	},
-	plugins: [
-    require('tailwindcss-opentype')
-  ],
-}
+  },
+  plugins: [require("tailwindcss-opentype")],
+};
