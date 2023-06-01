f307adaa0a33678b3bab622ba1729ea4e980570a
Robert Rosenberg <robdrosenberg@gmail.com>
2023-06-01 14:46:24 -0700

    nav fixes

--- src/components/MobileMenu.svelte
+++ src/components/MobileMenu.svelte
@@ -29,13 +29,13 @@
       </li>
     </ul>
 
-    <div class="flex flex-col gap-4">
+    <!-- <div class="flex flex-col gap-4">
       <p class="text-sm">LANGUAGE</p>
       <div class="flex gap-8">
         <button class="  font-mack text-2xl">EN</button>
         <button class=" font-mack text-2xl">SK</button>
       </div>
-    </div>
+    </div> -->
     <!-- svelte-ignore a11y-img-redundant-alt -->
     <div class="absolute bottom-0 right-5 z-50 object-cover p-1 pb-8 bg-white">
       <img
